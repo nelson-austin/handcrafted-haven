142c49ca4a249b09a1a74e4e78689355cd1bf9d5
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-16 19:55:29 +0200

    .

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,10 +1,14 @@
-import type { Metadata } from "next";
-import { Header } from "../ui/products/addToCartButton";
+"use client";
+// import type { Metadata } from "next";
+import Header from "../ui/products/header";
+import store from "../../redux/store";
+import { Provider } from "react-redux";
+import styles from "../page.module.css";
 
-export const metadata: Metadata = {
-  title: "Login",
-  description: "Login into Handcrafted Haven or Sign Up",
-};
+// export const metadata: Metadata = {
+//   title: "Login",
+//   description: "Login into Handcrafted Haven or Sign Up",
+// };
 
 export default function RootLayout({
   children,
@@ -13,10 +17,14 @@ export default function RootLayout({
 }>) {
   return (
     <>
-      <div className="h-25 shrink-0 bg-green-900 rounded-lg m-3 md:h-28">
-        <Header />
-      </div>
-      <main>{children}</main>
+      <Provider store={store}>
+        <div
+          className={`${styles.fixedHeader} sticky top h-25 bg-green-900 rounded-lg m-3 md:h-28 `}
+        >
+          <Header />
+        </div>
+        <main>{children}</main>
+      </Provider>
     </>
   );
 }
--- src/app/ui/products/addToCartButton.tsx
+++ src/app/ui/products/addToCartButton.tsx
@@ -1,49 +1,26 @@
+"use client";
 import Link from "next/link";
-import Image from "next/image";
-
-export const Header = () => {
-  return (
-    <header className="shrink-0 bg-green-900 rounded-lg md:h-28">
-      <div className="header-items flex items-center justify-between">
-        <div className="flex flex-col">
-          <Link href={"/"}>
-            <div className="flex items-center">
-              <Image
-                src={"/logo-placeholder-image.png"}
-                width={100}
-                height={100}
-                alt="Logo Image"
-              />
-              <p className="text-sky-100 text-[26px] font-bold leading-none">
-                Handcrafted{" "}
-                <span className="text-[51px] md:text-[26px]">Haven</span>
-              </p>
-            </div>
-          </Link>
-        </div>
-
-        <div className="text-sky-400 font-semibold leading-none mr-4 md:ml-[270px] md:hover:text-sky-100 lg:ml-[900px]">
-          <Link
-            href={"/cart"}
-            className="text-[23px] flex flex-col items-center"
-          >
-            CART{" "}
-            <span className="cart-badge text-orange-400 text-[23px]">00</span>
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-};
+import { useDispatch } from "react-redux";
+import { incrementCartCount } from "@/redux/features/cartCounterSlice";
 
 export default function AddToCartButton() {
+  const dispatch = useDispatch();
+
+  const handleCartCounter = () => {
+    dispatch(incrementCartCount());
+  };
   return (
     <>
-      <div className="bg-green-800 my-3 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100">
-        <Link href={"/dashboard/products/addcart"}>
-          <button className="">Add to Cart</button>
-        </Link>
-      </div>
+      {/* <Link href={"/#"}> */}
+
+      <button
+        onClick={handleCartCounter}
+        className="bg-green-800 m-3 w-80 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100 md:w-[200px] lg:w-[270px]"
+      >
+        Add to Cart
+      </button>
+
+      {/* </Link> */}
     </>
   );
 }
