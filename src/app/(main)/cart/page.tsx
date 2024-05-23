7ab25a01869caf7bf18df6e4a2b3ac5fb6b944a2
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-23 22:25:14 +0200

    .

--- src/app/(main)/cart/page.tsx
+++ src/app/(main)/cart/page.tsx
@@ -18,7 +18,7 @@ export default function CartPage() {
   };
 
   const handleIncrement = (id: string) => {
-    const updatedCartItems = cartItems.map((item: any) => {
+    const updatedCartItems = cartItems.map((item: CartItem) => {
       if (item.id === id) {
         return {
           ...item,
@@ -29,12 +29,12 @@ export default function CartPage() {
       return item;
     });
 
-    localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+    updateLocalStorage(updatedCartItems);
     dispatch(setCartItems(updatedCartItems));
   };
 
   const handleDecrement = (id: string) => {
-    const updatedCartItems = cartItems.map((item: any) => {
+    const updatedCartItems = cartItems.map((item: CartItem) => {
       if (item.id === id && item.quantity > 1) {
         return {
           ...item,
@@ -44,26 +44,27 @@ export default function CartPage() {
       return item;
     });
 
-    localStorage.setItem("cart-items", JSON.stringify(updatedCartItems));
+    updateLocalStorage(updatedCartItems);
     dispatch(setCartItems(updatedCartItems));
   };
 
   const handleRemove = (id: string) => {
-    dispatch(removeItemFromCart(id));
-
-    const updatedCartItems = cartItems.filter((item: any) => item.id !== id);
+    const updatedCartItems = cartItems.filter(
+      (item: CartItem) => item.id !== id
+    );
     updateLocalStorage(updatedCartItems);
+    dispatch(removeItemFromCart(id));
   };
 
   const totalCost = cartItems.reduce(
-    (accumulator: number, currentItem: any) =>
+    (accumulator: number, currentItem: CartItem) =>
       accumulator + currentItem.price * currentItem.quantity,
     0
   );
 
   return (
     <section className="pt-[150px] pb-20">
-      <h2 className="text-center  text-[33px] font-bold">Shopping Cart</h2>
+      <h2 className="text-center text-[33px] font-bold">Shopping Cart</h2>
       {cartItems.length === 0 ? (
         <div className="text-center text-[36px]">
           <p>Your cart is empty.</p>
@@ -73,13 +74,13 @@ export default function CartPage() {
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-6 h-6"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M9 15 3 9m0 0 6-6M3 9h12a6 6 0 0 1 0 12h-3"
                 />
               </svg>
@@ -90,10 +91,10 @@ export default function CartPage() {
       ) : (
         <div>
           <div className="flex flex-col items-center md:grid place-items-center grid-cols-2 lg:grid-cols-3">
-            {cartItems.map((item: any) => (
+            {cartItems.map((item: CartItem) => (
               <div
                 key={item.id}
-                className="flex flex-col m-3 items-center bg-blue-100 shadow-lg rounded-lg p-4 w-[363px] md:w-[357px] lg:w-[390px] "
+                className="flex flex-col m-3 items-center bg-blue-100 shadow-lg rounded-lg p-4 w-[363px] md:w-[357px] lg:w-[390px]"
               >
                 <img
                   src={`/products/${item.image}`}
@@ -113,13 +114,13 @@ export default function CartPage() {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M12 4.5v15m7.5-7.5h-15"
                       />
                     </svg>
@@ -132,13 +133,13 @@ export default function CartPage() {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                         d="M5 12h14"
                       />
                     </svg>
@@ -151,14 +152,14 @@ export default function CartPage() {
                       xmlns="http://www.w3.org/2000/svg"
                       fill="none"
                       viewBox="0 0 24 24"
-                      stroke-width="1.5"
+                      strokeWidth="1.5"
                       stroke="currentColor"
                       className="w-6 h-6"
                     >
                       <path
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
-                        d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                       />
                     </svg>
                   </button>
@@ -179,7 +180,7 @@ export default function CartPage() {
             </div>
             <div className="text-[36px]">
               <Link href={"/"}>
-                <div className="flex items-center justify-center md:justify-end gap-1 pt-4 text-gray-400 md:mr-5  md:hover:text-gray-600">
+                <div className="flex items-center justify-center md:justify-end gap-1 pt-4 text-gray-400 md:mr-5 md:hover:text-gray-600">
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="none"
