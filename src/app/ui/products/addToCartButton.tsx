fe2c284e49cd485fd8a3812eb198b976e3b2d7b6
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-23 11:11:34 +0200

    .

--- src/app/ui/products/addToCartButton.tsx
+++ src/app/ui/products/addToCartButton.tsx
@@ -1,8 +1,12 @@
 "use client";
 
 import { useDispatch } from "react-redux";
-import { incrementItemQuantity, addItemToCart } from "@/redux/features/cartCounterSlice";
+import {
+  incrementItemQuantity,
+  addItemToCart,
+} from "@/redux/features/cartCounterSlice";
 import { Product } from "@/app/lib/interface";
+import Link from "next/link";
 
 interface AddToCartButtonProps {
   product: Product;
@@ -14,7 +18,7 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
   const handleAddToCart = () => {
     // Increment cart count in Redux state
     dispatch(addItemToCart(product));
-    dispatch(incrementItemQuantity(product.id))
+    dispatch(incrementItemQuantity(product.id));
 
     // Retrieve the existing cart items from local storage and parse them
     const existingCartItems = JSON.parse(
@@ -30,12 +34,14 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
 
   return (
     <>
-      <button
-        onClick={handleAddToCart}
-        className="bg-green-800 m-3 w-80 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100 md:w-[200px] lg:w-[270px]"
-      >
-        Add to Cart
-      </button>
+      <Link href={"/cart"}>
+        <button
+          onClick={handleAddToCart}
+          className="bg-green-800 m-3 w-80 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100 md:w-[200px] lg:w-[270px]"
+        >
+          Add to Cart
+        </button>
+      </Link>
     </>
   );
 }
