92b91a88877a4d74d94f601bd9b90ee719012362
St3ampunk3d <mysteriousmr.beale@gmail.com>
2024-05-24 18:43:17 +0300

    Added hyperlink to product cards

--- src/app/ui/products/productCard.tsx
+++ src/app/ui/products/productCard.tsx
@@ -1,4 +1,5 @@
 import Image from "next/image";
+import Link from "next/link";
 import { Product } from "@/app/lib/interface";
 import AddToCartButton from "./addToCartButton";
 
@@ -10,24 +11,28 @@ export default function ProductCard({
   description,
   user_id,
   quantity_available,
+  sold_out,
 }: Product) {
   const imgPath = `/products/${image}`;
+  const productPage = `/product/${id}`
 
   return (
     <div className="flex flex-col rounded-xl items-center justify-center p-3 bg-blue-100">
       <div className="max-w-sm rounded-xl overflow-hidden shadow-lg mx-4 my-4">
-        <Image
-          src={imgPath}
-          width={150}
-          height={250}
-          alt="Product Image"
-          className="w-[330px] m-auto rounded-xl md:w-[200px]"
-        />
-        <div className="px-6 py-4">
-          <div className="font-bold text-xl mb-2">{name}</div>
-          <p className="text-gray-700 text-base">{description}</p>
-          <p className="text-gray-700 text-base font-semibold">${price}</p>
-        </div>
+        <Link href={productPage}>
+          <Image
+            src={imgPath}
+            width={150}
+            height={250}
+            alt="Product Image"
+            className="w-[330px] m-auto rounded-xl md:w-[200px]"
+          />
+          <div className="px-6 py-4">
+            <div className="font-bold text-xl mb-2">{name}</div>
+            <p className="text-gray-700 text-base">{description}</p>
+            <p className="text-gray-700 text-base font-semibold">${price}</p>
+          </div>
+        </Link>
         <AddToCartButton
           product={{
             id,
@@ -37,6 +42,7 @@ export default function ProductCard({
             description,
             user_id,
             quantity_available,
+            sold_out,
           }}
         />
       </div>
