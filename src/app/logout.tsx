3cffa2c041594ecad54240ff93f56343496d7a0c
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-28 22:41:15 +0200

    Redirect to home page after signing out modified.

--- src/app/logout.tsx
+++ src/app/logout.tsx
@@ -1,11 +1,8 @@
 "use client";
 
 import { signOut } from "next-auth/react";
-import { useRouter } from "next/navigation";
 
 export default function Logout() {
-  const router = useRouter();
-
   const handleSignOut = () => {
     signOut({ callbackUrl: "/" });
   };
