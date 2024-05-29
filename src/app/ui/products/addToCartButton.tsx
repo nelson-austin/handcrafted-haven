69a6b7485b2d6d0228b5811d665d915c429d3ebb
Abdul Mahl <mdu_abdul@icloud.com>
2024-05-29 19:21:13 +0200

    Replaced add to cart link.

--- src/app/ui/products/addToCartButton.tsx
+++ src/app/ui/products/addToCartButton.tsx
@@ -25,11 +25,13 @@ export default function AddToCartButton({ product }: AddToCartButtonProps) {
   };
 
   return (
-    <button
-      onClick={handleAddToCart}
-      className="bg-green-800 m-3 w-80 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100 md:w-[200px] lg:w-[270px]"
-    >
-      Add to Cart
-    </button>
+    <Link href="/">
+      <button
+        onClick={handleAddToCart}
+        className="bg-green-800 m-3 w-80 text-2xl rounded-lg p-3 flex justify-center items-center md:hover:bg-green-700 text-sky-100 md:w-[200px] lg:w-[270px]"
+      >
+        Add to Cart
+      </button>
+    </Link>
   );
 }
