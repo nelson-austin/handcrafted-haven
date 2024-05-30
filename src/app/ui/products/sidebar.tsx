28bc80c145d2d18485b062a97457ba93c8055117
St3ampunk3d <mysteriousmr.beale@gmail.com>
2024-05-30 21:33:39 +0300

    Styled the filtering menu on the products page

--- src/app/ui/products/sidebar.tsx
+++ src/app/ui/products/sidebar.tsx
@@ -66,19 +66,21 @@ export default function CollapsibleMenu({ categories } : {categories: Category[]
 
   return (
     <div className="relative">
-      <button
-        onClick={toggleMenu}
-        className="bg-blue-500 text-white p-2 rounded-md w-full"
-      >
-        {isOpen ? 'Hide Filters' : 'Show Filters'}
-      </button>
-      {isOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-90 flex flex-col p-4 z-50">
+      <div className="flex">
         <button
           onClick={toggleMenu}
-          className="self-end mb-4 bg-red-500 text-white p-2 rounded-md"
+          className="ml-auto p-4"
         >
-          Close
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#000000" d="M3.9 54.9C10.5 40.9 24.5 32 40 32H472c15.5 0 29.5 8.9 36.1 22.9s4.6 30.5-5.2 42.5L320 320.9V448c0 12.1-6.8 23.2-17.7 28.6s-23.8 4.3-33.5-3l-64-48c-8.1-6-12.8-15.5-12.8-25.6V320.9L9 97.3C-.7 85.4-2.8 68.8 3.9 54.9z"/></svg>Filter
+        </button>
+      </div>
+      {isOpen && (
+        <div className="fixed top-0 left-0 w-full h-full bg-white bg-opacity-90 flex flex-col p-8 lg:p-48 z-50">
+        <button
+          onClick={toggleMenu}
+          className="self-end mb-4 p-2 rounded-md"
+        >
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="#CC3333" d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z"/></svg>Close
         </button>
           <div className="mb-4">
             <label className="block mb-2 text-sm font-medium text-gray-700">
@@ -87,6 +89,7 @@ export default function CollapsibleMenu({ categories } : {categories: Category[]
             <input
               type="text"
               value={searchPhrase}
+              placeholder="Search for..."
               onChange={(e) => {
                 setSearchPhrase(e.target.value);
                 handleSearch(e.target.value);
@@ -146,8 +149,14 @@ export default function CollapsibleMenu({ categories } : {categories: Category[]
             </div>
           </div>
           <button
+            onClick={toggleMenu}
+            className="bg-blue-500 text-white p-2 mb-4 ml-auto rounded-md w-full"
+          >
+            Apply Filters
+          </button>
+          <button
             onClick={resetFilters}
-            className="bg-red-500 text-white p-2 rounded-md w-full"
+            className="bg-red-500 text-white p-2 mt-4 ml-auto rounded-md w-full"
           >
             Reset Filters
           </button>
