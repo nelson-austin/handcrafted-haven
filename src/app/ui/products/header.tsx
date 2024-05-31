0bb2314f651e3dd3b1ae728a6c9da944fe6a1036
Austin Nelson <nel.austintyler.son@gmail.com>
2024-05-30 18:33:12 -0600

    decrease margin

--- src/app/ui/products/header.tsx
+++ src/app/ui/products/header.tsx
@@ -97,7 +97,7 @@ export default function Header() {
                 />
               </svg>
             </button>
-            <span className={`text-white font-semibold text-[20px] bg-red-900 p-1 rounded-[50%] w-[25px] h-[25px] flex items-center justify-center mb-5 -ml-10 mr-10 md:hidden
+            <span className={`text-white font-semibold text-[20px] bg-red-900 p-1 rounded-[50%] w-[25px] h-[25px] flex items-center justify-center mb-5 -ml-10 mr-5 md:hidden
               ${cartCount < 1 && 'invisible'}`}>
               {cartCount}
             </span>
