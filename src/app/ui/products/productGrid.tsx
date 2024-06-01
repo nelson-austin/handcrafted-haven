3b21d07fe477459105d4196a98b5ec57ea0e3995
Austin Nelson <nel.austintyler.son@gmail.com>
2024-06-01 14:42:36 -0600

    remove search bar

--- src/app/ui/products/productGrid.tsx
+++ src/app/ui/products/productGrid.tsx
@@ -1,5 +1,4 @@
 import { fetchFilteredProducts } from "@/app/lib/data";
-import Search from "@/app/ui/search";
 import ProductCard from "@/app/ui/products/productCard";
 
 export default async function ProductGrid({
@@ -23,9 +22,6 @@ export default async function ProductGrid({
 
   return (
     <main className="m-3">
-      <div className="">
-        <Search placeholder="Search products..." />
-      </div>
       <div className="pt-[15px] grid grid-cols-1 pb-20 w-[365px] m-auto gap-5 md:grid-cols-3 md:w-[750px] lg:w-[1405px] lg:grid-cols-4">
         {products.map((card) => {
           return <ProductCard key={card.id} {...card} />;
