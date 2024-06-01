62834c70f9c8ba57b71d725bb19b4c1418b095f1
St3ampunk3d <mysteriousmr.beale@gmail.com>
2024-06-01 10:50:33 +0300

    testing

--- src/app/dashboard/inventory/new-item/page.tsx
+++ src/app/dashboard/inventory/new-item/page.tsx
@@ -2,16 +2,14 @@ import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/lib/authOptions";
 import NewProductForm from '@/app/ui/dashboard/newProductForm'
 import { redirect } from 'next/navigation';
-import { fetchCategories } from "@/app/lib/data";
 
 
 export default async function Page() {
     const session = await getServerSession(authOptions);
-    const categories = await fetchCategories()
     if (session == null) {
         return redirect('/login');
     } else {
     const id = session.user.id;
-    return (<NewProductForm  id={id} categories={categories}/>);
+    return (<NewProductForm  id={id} />);
     }
 }
