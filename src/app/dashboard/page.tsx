c88aaf0ffdfddc7220064333a835dc3789665084
Samuele Leoni <leo22009@byui.edu>
2024-06-01 13:54:49 +0200

    hidden dashboard button in header for normal users + added dashboard authorization check

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,10 +1,20 @@
-
+import { auth } from "@/auth";
 import { Metadata } from "next";
+import { redirect } from "next/navigation";
 export const metadata: Metadata = {
-  title: "Home"
-}
+  title: "Home",
+};
 
-export default function Dashboard() {
+export default async function Dashboard() {
+  const session = await auth();
+  if (!session) {
+    redirect(`/login`);
+  } else {
+    if (!session.user.is_seller) {
+      redirect(`/profile/${session.user.id}`);
+    }
+  }
+
   return (
     <main>
       <p>Dashboard</p>
