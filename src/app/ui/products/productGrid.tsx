0ab5dd42b84fc0e83aa0f2733916c4333dc83f0c
Austin Nelson <nel.austintyler.son@gmail.com>
2024-06-05 22:40:48 -0600

    horizontal scroll bar

--- src/app/ui/products/productGrid.tsx
+++ src/app/ui/products/productGrid.tsx
@@ -22,7 +22,7 @@ export default async function ProductGrid({
 
   return (
     <main className="m-3">
-      <div className="pt-[15px] grid grid-cols-1 pb-20 w-[365px] m-auto gap-5 md:grid-cols-3 md:w-[750px] lg:w-[1405px] lg:grid-cols-4">
+      <div className="pt-[15px] grid grid-cols-1 pb-20 w-[365px] m-auto gap-5 md:grid-cols-3 md:w-full lg:grid-cols-4">
         {products.map((card) => {
           return <ProductCard key={card.id} {...card} />;
         })}
